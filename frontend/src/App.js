6a6f2ea012c6690e113884c6b98b7a69c5b73f7e
ReikoOchiai <lissage.23@gmail.com>
2022-02-26 22:01:14 +1100

    Creating Sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Login from "./Views/Login/login";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./Views/sidebar/sidebar";
 
 function App() {
+  const { auth } = useSelector((state) => state);
+
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      {!auth.LoggedIn ? (
+        <Login />
+      ) : (
+        <>
+          <Sidebar />
+        </>
+      )}
+    </Router>
   );
 }
 
