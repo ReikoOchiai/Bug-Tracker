8e514d9e5795b360cd74776298da8f2cf0d0e25b
ReikoOchiai <lissage.23@gmail.com>
2022-03-13 13:02:14 +1100

    Connecting Dashboard cards to view bugs cards

--- frontend/src/Views/Components/Dashboard/card.jsx
+++ frontend/src/Views/Components/Dashboard/card.jsx
@@ -7,7 +7,11 @@ export default (props) => {
   const { level, color } = Priority(props.priority);
 
   return (
-    <div className="dashboard-card" style={{ color: color }}>
+    <div
+      className="dashboard-card"
+      onClick={props.clicked}
+      style={{ color: color }}
+    >
       <h2>Total: {level}</h2>
       <p>{props.count}</p>
     </div>
